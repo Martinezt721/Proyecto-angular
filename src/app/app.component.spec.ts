e264469eceb9287fb928efa16bfce95e195f2eb2
Martinezt721 <tomymartinezgerard@gmail.com>
2023-03-02 17:50:22 -0300

    Proyecto Angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'garcyecto'`, () => {
+  it(`should have as title 'Pryecto angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('garcyecto');
+    expect(app.title).toEqual('Proyecto angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('garcyecto app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Proyecto angular app is running!');
   });
 });
